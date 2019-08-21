67e673f9387a6f2cd5a4bcb3f54a2a828af4dc40
Roman Nevyantsev <kovin44@gmail.com>
2019-08-21 18:40:37 +0600

    add filter function to controller

--- backend/src/main/java/ru/kovin/it_lab_register/repositories/UserRepository.java
+++ backend/src/main/java/ru/kovin/it_lab_register/repositories/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.kovin.it_lab_register.entities.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByEmail(String email);
